3686ff476828cd532674944f573dfa529d371183
choihyoeun04 <choihyoeun04@gmail.com>
2023-05-27 20:06:40 +0900

    horizontal anlign for buttons

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -3,6 +3,10 @@ import HomeButton from "./HomeButton";
 import "./Header.css"
 import {Container, Row, Col, Navbar} from "react-bootstrap"
 
+/* <Row><img src="./images/background_church.jpg" alt="church" className="backgroundImg"/></Row> 
+이거왜이래??? 11 and none
+*/
+
 function Header() {
   function refreshPage(){
     window.location.reload();
@@ -15,7 +19,7 @@ function Header() {
           <Row className="headerRow">
             <Col className="navbarCol" onClick={refreshPage}>
               <h3 className="headerWebsiteName" >TECOW Finance Manager</h3>
-              <button className="homeButton11" path="/">testing</button>
+              <button className="homeButton" path="/">testing</button>
             </Col>
           </Row>
           <Row className="headerRow"> 
@@ -24,7 +28,7 @@ function Header() {
             <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Expenses" path="/expense"/></Col>
             <Col className="headerButtons"><HomeButton className="homeButton" buttonName="Login" path="/login"/></Col>
           </Row>
-          <Row><img src="./images/background_church.jpg" alt="church" className="backgroundImg"/></Row>
+          
         </Container>
       </Navbar>
       
