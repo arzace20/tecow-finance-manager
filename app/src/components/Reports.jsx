1f8d4e1996321ea91d4561df7882b82d5f0c4eea
choihyoeun04 <choihyoeun04@gmail.com>
2023-08-15 23:16:21 +0900

    feature/FM-21 : Fixed table

--- app/src/components/Reports.jsx
+++ app/src/components/Reports.jsx
@@ -5,64 +5,6 @@ import Table from 'react-bootstrap/Table';
 import "./Reports.css"
 import React, { useEffect, useState } from 'react';
 
-/*
-function ReportPage() {
-  const [reportData, setReportData] = useState([]);
-
-  const handleReportClick = async () => {
-    try {
-      const response = await axios.get('/report');
-      console.log(response);
-      setReportData(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div className='reportDiv'>
-      <Button className='ReportButton'>Monthly Report</Button>
-      <Button className='ReportButton' onClick={handleReportClick}>Generate Report</Button>
-      <Table striped bordered hover className='reportTable'>
-        <thead>
-          <tr>
-            <th className='TableTypes'>Member ID</th>
-            <th className='TableTypes'>Weekly Offering</th>
-            <th className='TableTypes'>Tithe</th>
-            <th className='TableTypes'>Special</th>
-            <th className='TableTypes'>Building</th>
-            <th className='TableTypes'>Misc</th>
-            <th className='TableTypes'>Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reportData.map((row) => (
-            <tr key={row._id}>
-              <td>{row._id}</td>
-              <td>{row.weekly}</td>
-              <td>{row.tithe}</td>
-              <td>{row.special}</td>
-              <td>{row.buldingFund}</td>
-              <td>{row.misc}</td>
-              <td>
-                {[
-                  row.weekly,
-                  row.tithe,
-                  row.special,
-                  row.buildingFund,
-                  row.misc,
-                ].reduce((acc, cur) => acc + cur, 0)}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
-  );
-}
-
-export default ReportPage;
-*/
 
 const ReportPage = () => {
 
